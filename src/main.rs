07cc932653ba367cf67b9faaa206a0c4950ba5bc
Skyler Ferris <skyvine@protonmail.com>
2025-06-19 10:40:45 -0700

    Add is_alive endpoint & run server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+/// An endpoint to checks that the server is up.
+/// 
+/// This endpoint always responds with status OK simply to verify that
+/// the server is running and responding to requests.
+#[get("/is_alive")]
+async fn is_alive() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+        .service(is_alive)
+    })
+    .bind(("127.0.0.1", 8123))?
+    .run()
+    .await
 }
