336a9b257f36afb876ee709f5e0745cd64e2255e
Skyler Ferris <skyvine@protonmail.com>
2025-06-19 12:57:56 -0700

    Add CLI arguments for IP address & port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use clap::Parser;
 use rust_todo::run;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    /// The IP address to listen on
+    #[arg(short, long, default_value_t = String::from("127.0.0.1"))]
+    ip_address: String,
+
+    #[arg(short, long, default_value_t = 8000)]
+    /// The port to listen on
+    port: u16,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    run(String::from("127.0.0.1"), 8123).await
+    let arguments = Arguments::parse();
+
+    run(arguments.ip_address, arguments.port).await
 }
