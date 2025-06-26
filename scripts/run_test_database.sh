b50b5f43edaaf064bd370aac904d0b953e036cb8
Skyler Ferris <skyvine@protonmail.com>
2025-06-26 15:50:04 -0700

    Don't detach the CREATE DATABASE command so output is visible.

--- scripts/run_test_database.sh
+++ scripts/run_test_database.sh
@@ -6,4 +6,4 @@ POSTGRES_VERSION=16
 
 docker run --rm -e POSTGRES_PASSWORD="$PASSWORD" --publish 5432:5432 --detach --tty postgres:"$POSTGRES_VERSION"
 sleep 2 # wait for service to be up
-docker run --rm --network host --detach --tty postgres:"$POSTGRES_VERSION" psql postgres://postgres:test-password@localhost -c "CREATE DATABASE todo;"
+docker run --rm --network host --tty postgres:"$POSTGRES_VERSION" psql postgres://postgres:test-password@localhost -c "CREATE DATABASE todo;"
