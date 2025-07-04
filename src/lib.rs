f85a5be29fc3f595a0b7e5941f28e836012c3e1a
Skyler Ferris <skyvine@protonmail.com>
2025-07-04 09:11:45 -0700

    Add check to avoid duplicate usernames.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ mod schema;
 use actix_web::{get, post, App, HttpResponse, HttpServer, Responder};
 use diesel::prelude::*;
 use dotenvy::dotenv;
+use schema::users;
 use std::env;
 use tracing::{event, span, Level};
 use uuid::Uuid;
@@ -15,6 +16,14 @@ use uuid::Uuid;
 async fn is_alive() -> impl Responder {
     event!(Level::TRACE, "Responding to is_alive check");
     HttpResponse::Ok()
+}
+
+#[derive(Queryable, Selectable)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+struct User {
+    id: i32,
+    username: String,
+    password: String,
 }
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -37,6 +46,13 @@ fn establish_connection() -> Result<PgConnection, ConnectionError> {
     connection
 }
 
+fn user_exists(name: &String, connection: &mut PgConnection) -> Result<bool, diesel::result::Error> {
+    use self::schema::users::dsl::*;
+    let query = users.filter(username.eq(name)).select(User::as_select());
+    event!(Level::TRACE, "Running query: {}", diesel::debug_query::<diesel::pg::Pg, _>(&query));
+    Ok(query.load(connection)?.len() >= 1)
+}
+
 #[post("/register_account")]
 async fn register_account(mut account_info: actix_web::web::Json<NewAccountInfo>) -> impl Responder {
     use self::schema::users::dsl::*;
@@ -54,22 +70,34 @@ async fn register_account(mut account_info: actix_web::web::Json<NewAccountInfo>
             let un = std::mem::take(&mut account_info.name);
             let pw = std::mem::take(&mut account_info.password);
 
-            let query = diesel::insert_into(users).values((username.eq(un), password.eq(pw)));
+            match user_exists(&un, &mut conn) {
+                Ok(false) => {
+                    let query = diesel::insert_into(users).values((username.eq(un), password.eq(pw)));
 
-            event!(Level::TRACE, "Running query: {}", diesel::debug_query::<diesel::pg::Pg, _>(&query));
+                    event!(Level::TRACE, "Running query: {}", diesel::debug_query::<diesel::pg::Pg, _>(&query));
 
-            let result = query.execute(&mut conn);
-            match result {
-                Ok(_) => {
-                    event!(Level::TRACE, "Query succeeded");
-                    HttpResponse::Ok().finish()
+                    let result = query.execute(&mut conn);
+                    match result {
+                        Ok(_) => {
+                            event!(Level::TRACE, "Query succeeded");
+                            HttpResponse::Ok().finish()
+                        },
+                        Err(e) => {
+                            event!(Level::ERROR, "Query failed: {e}");
+                            HttpResponse::InternalServerError().body(format!("Request ID: {request_id}"))
+                        }
+                    }
                 },
+
+                Ok(true) => HttpResponse::Conflict().body(format!("Request ID: {request_id}")),
+
                 Err(e) => {
-                    event!(Level::ERROR, "Query failed: {e}");
+                    event!(Level::ERROR, "Unable to query database for existing users: {e}");
                     HttpResponse::InternalServerError().body(format!("Request ID: {request_id}"))
                 }
             }
-        }
+        },
+
         Err(e) => {
             event!(Level::ERROR, "Unable to establish connection to database: {e}.");
             HttpResponse::InternalServerError().body(format!("Request ID: {request_id}"))
@@ -96,7 +124,12 @@ mod tests {
 
     #[ctor]
     unsafe fn global_init() {
+        use super::schema::users::dsl::*;
+        use super::RunQueryDsl;
+
         fmt().event_format(fmt::format().pretty()).init();
+
+        diesel::delete(users).execute(&mut super::establish_connection().expect("Unable to connect to test database."));
     }
 
     #[actix_web::test]
@@ -121,6 +154,36 @@ mod tests {
             let formatted_response = format!("{response:?}");
             let body = response.into_body();
             panic!("Response indicated failure: {formatted_response}{body:?}")
+        }
+    }
+
+    #[actix_web::test]
+    async fn duplicate_username_fails() {
+        let name = String::from("duplicated-name");
+        let password = String::from("duplicated-password");
+
+        let app = test::init_service(App::new().service(super::register_account)).await;
+        let first_request = test::TestRequest::post().uri("/register_account").set_json(super::NewAccountInfo {
+            name:     name.clone(),
+            password: password.clone(),
+        }).to_request();
+
+        let first_response = test::call_service(&app, first_request).await;
+        if !first_response.status().is_success() {
+            let formatted_response = format!("{first_response:?}");
+            let body = first_response.into_body();
+            panic!("Response indicated failure: {formatted_response}{body:?}")
+        }
+
+        let second_request = test::TestRequest::post().uri("/register_account").set_json(super::NewAccountInfo {
+            name,
+            password,
+        }).to_request();
+        let second_response = test::call_service(&app, second_request).await;
+        if !second_response.status().is_client_error() {
+            let formatted_response = format!("{second_response:?}");
+            let body = second_response.into_body();
+            panic!("Response indicated success: {formatted_response}{body:?}")
         }
     }
 }
